51c5de684444172f11a8f0ba5fb394b533aceb30
protoblaster <120064629+protoblaster@users.noreply.github.com>
2023-01-23 22:23:50 +0000

    added themes from DT's <DistroTube> build of DMenu

---
+++ colorschemes/nord.h
@@ -0,0 +1,11 @@
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#D8DEE9", "#2E3440" },
+	[SchemeSel] = { "#000000", "#B48EAD" },
+	[SchemeSelHighlight] = { "#A3BE8C", "#000000" },
+	[SchemeNormHighlight] = { "#A3BE8C", "#000000" },
+	[SchemeOut] = { "#2E3440", "#88C0D0" },
+	[SchemeOutHighlight] = { "#ffc978", "#00ffff" },
+	[SchemeMid] = { "#D8DEE9", "#000000" },
+};
+
